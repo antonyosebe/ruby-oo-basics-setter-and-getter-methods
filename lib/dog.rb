820dc22a71a06f940854bb7b1828082106f25ffa
antonyosebe <osebe.anto@gmail.com>
2023-02-10 21:13:41 +0300

    complete  assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,20 @@
 class Dog
+    # getter
+    def name=(name)
+        @name=name
+    end
+    # setter
+    def name
+        @name
+    end
+    # getter
+    def breed=(breed)
+        @breed=breed
+    end
+    # setter
+    def breed
+        @breed
+    end
+
 
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,26 @@
+require "pry"
 class Person
+    # getter
+    def name=(name)
+        @name=name
+    end
+    # setter
+    def name
+        @name
+    end
+    # getter
+    def job=(job)
+        @job=job
+    end
+    # setter
+    def job
+        @job
+    end
+
 
 end
+# binding.pry
+
+kanye = Person.new
+kanye.name = "Kanye"
+kanye.name
